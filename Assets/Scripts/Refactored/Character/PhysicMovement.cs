51c12fb9820cb566020063604968ace047300468
Anton Siadun <antochaleo@gmail.com>
2022-11-17 15:54:52 +0300

    feat: add physic movement component
    It's a concrete realization of IMovement, that provides movement functionality.
    Component must secure on a gameobject, that contains Rigidbody2D component.

---
+++ Assets/Scripts/Refactored/Character/PhysicMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace AntonSiadun.StickyWallsProto.Domain.Movement
+{
+    public class PhysicMovement : MonoBehaviour, IMovement
+    {
+        public Vector2 Direction => _direction;
+
+        [SerializeField] private Vector2 _force = new Vector2(0f, 10f);
+
+        private Rigidbody2D _rb;
+        private Vector2 _direction = Vector2.right;
+
+        private void Awake()
+        {
+            if (TryGetComponent(out Rigidbody2D rb))
+            {
+                _rb = rb;
+            }
+            else
+            {
+                throw new MissingComponentException();
+            }
+        }
+
+        public void Jump(float multipier)
+        {
+            Vector2 force = multipier * new Vector2(_force.x * _direction.x, _force.y);
+            _rb.AddForce(multipier * force);
+        }
+
+        public void TurnBack()
+        {
+            _direction *= -1;
+        }
+
+        public void ResetVelocity()
+        {
+            _rb.velocity = new Vector2(0f, 0f);
+        }
+    }
+}
