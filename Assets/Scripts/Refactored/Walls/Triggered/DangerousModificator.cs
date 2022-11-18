7c846b6b068b8b52eaf4c0974ada0668354f707b
Anton Siadun <antochaleo@gmail.com>
2022-11-18 14:36:29 +0300

    feat: add dangerous modificator
    A reactive component, that reacts to collision and change a state in the provider.

---
+++ Assets/Scripts/Refactored/Walls/Triggered/DangerousModificator.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using Zenject;
+using System;
+
+namespace AntonSiadun.StickyWallsProto.Domain.Interactions.Triggered
+{
+    public class DangerousModificator : ReactiveComponent
+    {
+        [SerializeField] private string _tag;
+
+        private IGameStateProvider _provider;
+
+        [Inject]
+        public void Initialize(IGameStateProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public override void OnEnter(GameObject anObject)
+        {
+            if( anObject.CompareTag(_tag) )
+            {
+                _provider.SetState(GameState.Dead);
+            }
+        }
+
+        public override void OnExit(GameObject anObject) { }
+
+        public override void OnStay(GameObject anObject) { }
+    }
+}
