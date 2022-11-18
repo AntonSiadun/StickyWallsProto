cce2184b50d69a70cad513d0a46eaad692eafb2d
Anton Siadun <antochaleo@gmail.com>
2022-11-18 07:21:55 +0300

    feat: add elastic wall
    -//-

---
+++ Assets/Scripts/Refactored/Walls/Triggered/ElascticModificator.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using Zenject;
+using AntonSiadun.StickyWallsProto.Domain.Movement;
+
+namespace AntonSiadun.StickyWallsProto.Domain.Interactions.Triggered
+{
+    public class ElascticModificator : ReactiveComponent
+    {
+        [SerializeField] private float _pushForceMultipier;
+        [SerializeField] private string _tag;
+
+        private ICharacter _character;
+
+        [Inject]
+        public void Initialize(ICharacter character)
+        {
+            _character = character;
+        }
+
+        public override void OnEnter(GameObject anObject)
+        {
+            if(anObject.CompareTag(_tag))
+            {
+                _character.Stop();
+                _character.TurnBack();
+                _character.Pulse(_pushForceMultipier);
+            }
+        }
+
+        public override void OnExit(GameObject anObject)
+        {
+            if (anObject.CompareTag(_tag))
+            {
+                _character.Restore();
+            }
+        }
+
+        public override void OnStay(GameObject anObject)
+        {
+
+        }
+    }
+}
