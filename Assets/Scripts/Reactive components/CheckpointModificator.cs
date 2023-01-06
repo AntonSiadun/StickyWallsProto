4cba79b44dcc502bdef16c8f5c277d776e41a010
Anton Siadun <antochaleo@gmail.com>
2023-01-06 15:04:18 +0300

    fix: add spawn position checking when touch checkpoint
    -//-

--- Assets/Scripts/Reactive components/CheckpointModificator.cs
+++ Assets/Scripts/Reactive components/CheckpointModificator.cs
@@ -6,12 +6,14 @@ using Domain.Interactions.Triggered;
 public class CheckpointModificator : ReactiveComponent
 {
     private LevelGenerator _generator;
+    private Respawner _respawner;
     private bool _isUsed = false;
 
     [Inject]
-    public void Initialize(LevelGenerator generator)
+    public void Initialize(LevelGenerator generator, Respawner respawner)
     {
         _generator = generator ?? throw new NullReferenceException("Level generator is empty.");
+        _respawner = respawner ?? throw new NullReferenceException("Respawner is empty.");
     }
 
     public override void OnEnter(GameObject anObject)
@@ -19,6 +21,7 @@ public class CheckpointModificator : ReactiveComponent
         if (!_isUsed)
         {
             _generator.GenerateLevelPart(transform.position);
+            _respawner.SpawnPosition = transform.position + new Vector3(0.1f, 0f, 0f);
             _isUsed = true;
         }
     }
