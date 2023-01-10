c52a212362a7591c7a3971453f2561a9d5aac36a
Anton Siadun <antochaleo@gmail.com>
2023-01-10 22:02:52 +0300

    fix: move CameraFollow method from fixed to update
    For smooth drawing.

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -5,7 +5,7 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Transform _character;
     [SerializeField] private float _smooth = 0.5f;
 
-    private void FixedUpdate()
+    private void Update()
     {
         var characterRelative = new Vector3(_character.position.x,
                                             _character.position.y,
