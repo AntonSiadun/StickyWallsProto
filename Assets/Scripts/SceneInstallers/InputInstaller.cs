bc566706fea4c5da8add52a958e1a81f9fbb7ef3
Anton Siadun <antochaleo@gmail.com>
2023-01-10 22:39:56 +0300

    chore: setup scene params
    -//-

--- Assets/Scripts/SceneInstallers/InputInstaller.cs
+++ Assets/Scripts/SceneInstallers/InputInstaller.cs
@@ -5,7 +5,7 @@ public class InputInstaller : MonoInstaller
 {
     public override void InstallBindings()
     {
-        Container.Bind<IConcreteInputSignature>().To<PCInputSignature>().AsSingle();
+        Container.Bind<IConcreteInputSignature>().To<MobileInputSignature>().AsSingle();
         Container.Bind<IInput>().To<InputController>().FromNewComponentOnNewGameObject().AsSingle();
     }
 }
