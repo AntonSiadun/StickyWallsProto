5cbcc95b852267652497bc9247fc36a04deac986
Anton Siadun <antochaleo@gmail.com>
2023-02-24 22:53:55 +0300

    feat: move level generation to asset reference
    Because all prefabs moved to asset reference, level generator need to move in that way to work properly.

--- Assets/Scripts/Level generation/LevelGenerator.cs
+++ Assets/Scripts/Level generation/LevelGenerator.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using System.Threading.Tasks;
 
 public class LevelGenerator : MonoBehaviour
 {
@@ -15,17 +18,33 @@ public class LevelGenerator : MonoBehaviour
 
     [Header("Prefabs")]
     [SerializeField] private List<Element> _elements;
-    [SerializeField] private GameObject _checkpointPrefab;
-    [SerializeField] private GameObject _coinPrefab;
+    [SerializeField] private AssetReference _checkpointPrefab;
+    [SerializeField] private AssetReference _coinPrefab;
 
     [Header("Start elements")]
     [SerializeField] private GameObject _startElements;
 
-    private List<GameObject> _lastSpawned = new List<GameObject>();
+    [Header("Spawned")]
+    [SerializeField] private List<GameObject> _lastSpawned = new List<GameObject>();
     private Queue<GameObject> _checkpoints = new Queue<GameObject>();
     private INextPointGenerator _nextPointGenerator;
 
-    void Start()
+    private async void Awake()
+    {
+        var loadingTasks = new List<Task>();
+        foreach(var elem in _elements)
+        {
+            loadingTasks.Add( elem.Wall.LoadAssetAsync<GameObject>().Task);
+        }
+
+        loadingTasks.Add(_coinPrefab.LoadAssetAsync<GameObject>().Task);
+        loadingTasks.Add(_checkpointPrefab.LoadAssetAsync<GameObject>().Task);
+
+        await Task.WhenAll(loadingTasks.ToArray());
+        SetupLevel();
+    }
+
+    private void SetupLevel()
     {
         _nextPointGenerator = new AngleNextPointGenerator(_angle);
         GenerateLevelPart(Vector3.zero);
@@ -48,20 +67,28 @@ public class LevelGenerator : MonoBehaviour
             var distance = Random.Range(_min, _max);
             nextPosition = _nextPointGenerator.Get(nextPosition, distance);
             var element = _elements[Random.Range(0,_elements.Count)];
-            _lastSpawned.Add(Instantiate(element.Wall, nextPosition + element.Start, Quaternion.identity));
+
+            _lastSpawned.Add(SpawnElement(element.Wall, nextPosition + element.Start));
             nextPosition += element.End;
         }
         nextPosition = _nextPointGenerator.Get(nextPosition, Random.Range(_min, _max));
-        _checkpoints.Enqueue(Instantiate(_checkpointPrefab, nextPosition, Quaternion.identity));
+        _checkpoints.Enqueue((GameObject) Instantiate(_checkpointPrefab.Asset, nextPosition, Quaternion.identity));
     }
 
     private void DeleteLastElements()
     {
-        if(_checkpoints.Count > 1)
-            Destroy(_checkpoints.Dequeue());
-
-        foreach (var element in _lastSpawned)
-            Destroy(element);
+        if (_checkpoints.Count > 1)
+        {
+            var lastCheckpoint = _checkpoints.Dequeue();
+            if (!Addressables.ReleaseInstance(lastCheckpoint) && lastCheckpoint != null)
+                Destroy(lastCheckpoint);
+        }
+        
+        foreach (var anObject in _lastSpawned)
+        {
+            
+                Destroy(anObject);
+        }
         _lastSpawned.Clear();
     }
 
@@ -70,6 +97,22 @@ public class LevelGenerator : MonoBehaviour
         var randX = Random.Range(-_max, _max);
         var randY = Random.Range(0, _height);
         var offset = new Vector3(randX, randY, 0f);
-        _lastSpawned.Add(Instantiate(_coinPrefab, position + offset, Quaternion.identity));
+        _lastSpawned.Add((GameObject) Instantiate(_coinPrefab.Asset, position + offset, Quaternion.identity));
+    }
+
+    private GameObject SpawnElement(AssetReference reference, Vector3 position)
+    {
+        return (GameObject) Instantiate(reference.Asset, position, Quaternion.identity);
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var elem in _elements)
+        {
+            elem.Wall.ReleaseAsset();
+        }
+
+        _checkpointPrefab.ReleaseAsset();
+        _coinPrefab.ReleaseAsset();
     }
 }
